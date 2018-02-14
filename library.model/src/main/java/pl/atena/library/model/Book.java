fc8712149f5f53cd77eb415e1b9d38e56b1a0fc6
Aleksander.Lukjanczuk <Aleksander.Lukjanczuk@atena.pl>
2018-02-14 16:37:58 +0100

    Add class User

--- library.model/src/main/java/pl/atena/library/model/Book.java
+++ library.model/src/main/java/pl/atena/library/model/Book.java
@@ -7,10 +7,12 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.Table;
 import javax.persistence.Temporal;
 import javax.persistence.TemporalType;
 
 @Entity
+@Table(name = "BOOKS")
 public class Book {
 
 	@Id
@@ -37,6 +39,55 @@ public class Book {
 	public String toString() {
 		return String.format("Book [id=%s, title=%s, description=%s, publicationDate=%s, nrOfPages=%s, imgURL=%s]", id,
 				title, description, publicationDate, nrOfPages, imgURL);
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((description == null) ? 0 : description.hashCode());
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		result = prime * result + ((nrOfPages == null) ? 0 : nrOfPages.hashCode());
+		result = prime * result + ((publicationDate == null) ? 0 : publicationDate.hashCode());
+		result = prime * result + ((title == null) ? 0 : title.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Book other = (Book) obj;
+		if (description == null) {
+			if (other.description != null)
+				return false;
+		} else if (!description.equals(other.description))
+			return false;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		if (nrOfPages == null) {
+			if (other.nrOfPages != null)
+				return false;
+		} else if (!nrOfPages.equals(other.nrOfPages))
+			return false;
+		if (publicationDate == null) {
+			if (other.publicationDate != null)
+				return false;
+		} else if (!publicationDate.equals(other.publicationDate))
+			return false;
+		if (title == null) {
+			if (other.title != null)
+				return false;
+		} else if (!title.equals(other.title))
+			return false;
+		return true;
 	}
 
 	public Long getId() {
---
+++ library.model/src/main/java/pl/atena/library/model/User.java
@@ -0,0 +1,90 @@
+package pl.atena.library.model;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "USERS")
+public class User {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private Long id;
+
+	@Column(length = 20)
+	private String name;
+
+	@Column(length = 20)
+	private String surname;
+
+	@Override
+	public String toString() {
+		return String.format("User [id=%s, name=%s, surname=%s]", id, name, surname);
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		result = prime * result + ((name == null) ? 0 : name.hashCode());
+		result = prime * result + ((surname == null) ? 0 : surname.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		User other = (User) obj;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		if (name == null) {
+			if (other.name != null)
+				return false;
+		} else if (!name.equals(other.name))
+			return false;
+		if (surname == null) {
+			if (other.surname != null)
+				return false;
+		} else if (!surname.equals(other.surname))
+			return false;
+		return true;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getSurname() {
+		return surname;
+	}
+
+	public void setSurname(String surname) {
+		this.surname = surname;
+	}
+
+}
