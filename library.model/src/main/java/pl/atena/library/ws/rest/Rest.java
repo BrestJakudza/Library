44ae817fbc6649299eded6ea8bf142529fab5638
aleksander.lukjanczuk <BrestJakudza@gmail.com>
2018-06-10 15:37:28 +0200

    all in one .war project

---
+++ library.model/src/main/java/pl/atena/library/ws/rest/Rest.java
@@ -0,0 +1,9 @@
+package pl.atena.library.ws.rest;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+@ApplicationPath("/api")
+public class Rest extends Application {
+
+}
